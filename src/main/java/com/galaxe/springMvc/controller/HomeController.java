f4d1a0d450d4f097dc33407c8fa32f008e3f0aa0
Michael <mjbanvard@gmail.com>
2021-05-23 00:46:29 -0500

    Got HomeController to load. Moved to controller directory. Buttons do NOT function.

--- src/main/java/com/galaxe/springMvc/controller/HomeController.java
+++ src/main/java/com/galaxe/springMvc/controller/HomeController.java
@@ -1,36 +1,39 @@
 package com.galaxe.springMvc.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.galaxe.springMvc.domain.UserVO;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Handles requests for the application home page.
+ */
 @Controller
-@RequestMapping(value="/register/*")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value ="/home", method = RequestMethod.GET)
-	public ModelAndView home(ModelAndView mav) {
-		UserVO userVo = new UserVO();
-		userVo.setFirstName("Henry");
-		userVo.setLastName("Blinken");
-		userVo.setUsername("Admin");
-		userVo.setPassword("DFDFDF");
-		userVo.setRole("user");
-		userVo.setGender("Male");
-		mav.setViewName("home");
-		mav.addObject("userVo", userVo);
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		return mav;
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
 	}
 	
-	
-
 }
